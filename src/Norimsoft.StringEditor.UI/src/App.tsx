39b5cdba5d0238f217e2f56f60318823cd4c6bfd
Miron Jakubowski <mijakubowski@gmail.com>
2023-05-26 11:23:16 +0200

    Scaffold UI structure

--- src/Norimsoft.StringEditor.UI/src/App.tsx
+++ src/Norimsoft.StringEditor.UI/src/App.tsx
@@ -1,27 +1,32 @@
-import type { Component } from 'solid-js';
-
-import logo from './logo.svg';
-import styles from './App.module.css';
+import type {Component} from 'solid-js';
+import {Container, Nav, } from 'solid-bootstrap';
 
 const App: Component = () => {
-  return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <main>
+            <div class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark" style="width: 280px;">
+                <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+                    <span class="fs-4">String editor</span>
+                </a>
+                <hr></hr>
+                <Nav variant="pills" class="flex-column mb-auto">
+                    <Nav.Item>
+                        <Nav.Link class="text-white">
+                            <i class="bi bi-plus-lg"></i>
+                            <span>New app</span>
+                        </Nav.Link>
+                    </Nav.Item>
+                    <hr></hr>
+                    <Nav.Item>
+                        <Nav.Link eventKey="languages" class="text-white">Languages</Nav.Link>
+                    </Nav.Item>
+                </Nav>
+            </div>
+            <Container>
+                <p>This is content</p>
+            </Container>
+        </main>
+    );
 };
 
 export default App;
