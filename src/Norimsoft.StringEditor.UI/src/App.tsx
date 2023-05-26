a0d54adbc77878f8208e5816f65b88dc6972a3ae
Miron Jakubowski <mijakubowski@gmail.com>
2023-05-26 11:55:04 +0200

    Scaffold icons

--- src/Norimsoft.StringEditor.UI/src/App.tsx
+++ src/Norimsoft.StringEditor.UI/src/App.tsx
@@ -1,32 +1,35 @@
-import type {Component} from 'solid-js';
-import {Container, Nav, } from 'solid-bootstrap';
+import type { Component } from 'solid-js';
+import { Container, Nav } from 'solid-bootstrap';
+import Icon from './components/Icon';
 
 const App: Component = () => {
-    return (
-        <main>
-            <div class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark" style="width: 280px;">
-                <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-                    <span class="fs-4">String editor</span>
-                </a>
-                <hr></hr>
-                <Nav variant="pills" class="flex-column mb-auto">
-                    <Nav.Item>
-                        <Nav.Link class="text-white">
-                            <i class="bi bi-plus-lg"></i>
-                            <span>New app</span>
-                        </Nav.Link>
-                    </Nav.Item>
-                    <hr></hr>
-                    <Nav.Item>
-                        <Nav.Link eventKey="languages" class="text-white">Languages</Nav.Link>
-                    </Nav.Item>
-                </Nav>
-            </div>
-            <Container>
-                <p>This is content</p>
-            </Container>
-        </main>
-    );
+  return (
+    <main>
+      <div class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark" style="width: 280px;">
+        <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+          <span class="fs-4">String editor</span>
+        </a>
+        <hr></hr>
+        <Nav variant="pills" class="flex-column mb-auto">
+          <Nav.Item>
+            <Nav.Link class="text-white d-flex align-items-center">
+              <Icon name="bi-plus-lg" size={18} color="#fff" />
+              <span>New app</span>
+            </Nav.Link>
+          </Nav.Item>
+          <hr></hr>
+          <Nav.Item>
+            <Nav.Link eventKey="languages" class="text-white">
+              Languages
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </div>
+      <Container>
+        <p>This is content</p>
+      </Container>
+    </main>
+  );
 };
 
 export default App;
---
+++ src/Norimsoft.StringEditor.UI/src/components/Icon.tsx
@@ -0,0 +1,19 @@
+﻿const DefaultSize = 16;
+const DefaultColor = '#000';
+
+export default function Icon(props: Props) {
+  const size = props.size ?? DefaultSize;
+  const color = props.color ?? DefaultColor;
+
+  return (
+    <svg class="bi" width={size} height={size} fill={color}>
+      <use xlink:href={`#${props.name}`}></use>
+    </svg>
+  );
+}
+
+type Props = {
+  name: string;
+  size?: number;
+  color?: string;
+};
